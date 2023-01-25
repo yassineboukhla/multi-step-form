98abad181924bb1d08787dd89a6e2b4da1f148f6
Yassine Boukhla <yassineboukhla2018@gmail.com>
2023-01-25 18:01:55 +0100

    Step 2 JS code fully functional!

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// All Elements used in the code below
+let plan1 = document.querySelector(".plan1");
+let plan2 = document.querySelector(".plan2");
+let plan3 = document.querySelector(".plan3");
+let plans = document.querySelectorAll(".plan-card-wrapper");
+
+let nameInp = document.querySelector(".name-input");
+let emailInp = document.querySelector(".email-input");
+let phoneInp = document.querySelector(".phone-input");
+
+let circle = document.querySelector(".circle");
+
 // Function responsible for going to the next step
 function nextStep(elem) {
   if (checkInput(elem)) {
@@ -73,9 +85,6 @@ function backStep(elem) {
 // Function for regex
 let validation = document.querySelector(".validation");
 function checkInput() {
-  let nameInp = document.querySelector(".name-input");
-  let emailInp = document.querySelector(".email-input");
-  let phoneInp = document.querySelector(".phone-input");
   // Regex provided by chatGPT (I added this comment to let you know in case you need any regex)
   // Just go and get it from chatgpt
   let regexName = /^[A-Za-z]+$/;
@@ -104,4 +113,47 @@ function checkInput() {
 // Function responsible for clearing errors
 function clearError() {
   validation.classList.add("hidden");
+}
+
+// Function responsible for changing circle (Left: Month plan, right: Year plan)
+function changePlan() {
+  // Changing from Monthly to Yearly Plan
+  if (circle.classList.contains("circle-left")) {
+    circle.classList.remove("circle-left");
+    circle.classList.add("circle-right");
+    plan1.children[1].children[1].textContent = "$90/yr";
+    plan2.children[1].children[1].textContent = "$120/yr";
+    plan3.children[1].children[1].textContent = "$150/yr";
+    plans.forEach((elem) => {
+      let node = document.createElement("p");
+      let textNode = document.createTextNode("2 months free");
+      node.appendChild(textNode);
+      node.classList.add("months-free");
+      elem.appendChild(node);
+    });
+  } // Changing from Yearly to Monthly Plan
+  else if (circle.classList.contains("circle-right")) {
+    circle.classList.remove("circle-right");
+    circle.classList.add("circle-left");
+    plan1.children[1].children[1].textContent = "$9/yr";
+    plan2.children[1].children[1].textContent = "$12/yr";
+    plan3.children[1].children[1].textContent = "$15/yr";
+    plans.forEach((elem) => {
+      elem.removeChild(elem.lastChild);
+    });
+  }
+}
+
+// Function responsible for selecting plan
+function selectPlan(elem) {
+  elem.classList.toggle("blue-border");
+  if (elem != plan1 && plan1.classList.contains("blue-border")) {
+    plan1.classList.remove("blue-border");
+  }
+  if (elem != plan2 && plan2.classList.contains("blue-border")) {
+    plan2.classList.remove("blue-border");
+  }
+  if (elem != plan3 && plan3.classList.contains("blue-border")) {
+    plan3.classList.remove("blue-border");
+  }
 }
